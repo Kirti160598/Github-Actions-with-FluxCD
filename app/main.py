3346911bf63e06295f836dd20a9e6994ae68f943
Kirti  She/her <72144141+Kirti160598@users.noreply.github.com>
2025-04-29 16:45:05 +0530

    Updated main.py

--- app/main.py
+++ app/main.py
@@ -1,23 +1,9 @@
-from flask import Flask, jsonify, render_template
-from datetime import datetime
-
+from flask import Flask
 app = Flask(__name__)
 
 @app.route('/')
-def home():
-    # Get current timestamp for dynamic content
-    current_time = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
-    
-    return render_template('index.html', time=current_time)
-
-@app.route('/healthz')
-def health():
-    # Health check for Kubernetes readiness
-    return jsonify(status="ok", time=datetime.utcnow().isoformat())
+def hello():
+    return "Hello from GitHub Actions + FluxCD! working fine"
 
 if __name__ == '__main__':
-    try:
-        app.run(host='0.0.0.0', port=5000)
-    except Exception as e:
-        print(f"Error starting the application: {e}")
-
+    app.run(host='0.0.0.0', port=5000)
